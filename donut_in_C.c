4db3eea580b46c566e23836639f5345aa5d24844
Madhav K <madhav.k.3012@gmail.com>
2024-09-09 20:22:53 +0530

    Setting up the base for the project

---
+++ donut_in_C.c
@@ -0,0 +1,18 @@
+/**
+ * @file donut_in_C.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-09-08
+ * @soure https://www.a1k0n.net/2011/07/20/donut-math.html
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world!");
+    return 0;
+}
